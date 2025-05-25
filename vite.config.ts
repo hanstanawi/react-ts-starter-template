7dea57018d56d062c28178b8762b9399f6b0f3d3
Hans Tanawi <hans.tanawi@coolblue.nl>
2025-05-25 19:03:35 +0200

    fix: resolve tailwind breaking changes

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,11 @@
 /// <reference types="vite/client" />
 
 import { defineConfig } from 'vite';
+import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
+  plugins: [react(), tsconfigPaths(), tailwindcss()],
 });
